f07fd0e5b864b0406c049d93898b0cf10fdedc7e
Yong Siang Low <lowyongsiang@gmail.com>
2018-02-26 20:35:33 +0800

    Added extra prompt in python program to differentiate the sensorTag data type. Answer F for fixed, M for in motion

--- formatHelper.py
+++ formatHelper.py
@@ -2,6 +2,7 @@ import os
 import re
 
 file = raw_input("Enter file name: ")
+type = raw_input("Is the sensor tag fixed or in motion: (Answer F/M) ")
 string = open(file).read()
 
 def getNodeID():
@@ -10,7 +11,7 @@ def getNodeID():
     new_str = string.split("Node ID: ")[1]
     temp = new_str.split(" ")[0]
     nodeID = temp.split()[0]
-    print nodeID
+#    print nodeID
     return nodeID
 
 def removeIntroduction():
@@ -51,7 +52,7 @@ def rejectLine(line):
     return boolean
 
 nodeID = getNodeID()
-final = open(str(nodeID) + '.csv', 'a')
+final = open(str(nodeID) + str(type.upper()) + '.csv', 'a')
 
 def main():
     with open(removeIntroduction()) as openfileobject:
