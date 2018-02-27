656878d1d07b16d4894f5c470e825d034fa74e92
Yong Siang Low <lowyongsiang@gmail.com>
2018-02-27 14:27:50 +0800

    Added the conversion part for formatHelper to convert G to Velocity

--- formatHelper.py
+++ formatHelper.py
@@ -4,6 +4,7 @@ import re
 file = raw_input("Enter file name: ")
 type = raw_input("Is the sensor tag fixed or in motion: (Answer F/M) ")
 string = open(file).read()
+numberOfSteps = raw_input("How many steps was this: ")
 
 def getNodeID():
     global string
@@ -11,7 +12,6 @@ def getNodeID():
     new_str = string.split("Node ID: ")[1]
     temp = new_str.split(" ")[0]
     nodeID = temp.split()[0]
-#    print nodeID
     return nodeID
 
 def removeIntroduction():
@@ -49,15 +49,24 @@ def rejectLine(line):
         boolean = True
     return boolean
 
-nodeID = getNodeID()
-final = open(str(nodeID) + str(type.upper()) + '.csv', 'a')
+def convertToVelocity(gValue):
+    result = int(gValue) * 9.80665
+    return result
+
+def getTupleVelocity(x,y,z):
+    return convertToVelocity(x), convertToVelocity(y), convertToVelocity(z)
+
+#nodeID = getNodeID()
+final = open(str(numberOfSteps) + "_" + str(type.upper()) + '.csv', 'a')
 
 def main():
     with open(removeIntroduction()) as openfileobject:
         for line in openfileobject:
             if not rejectLine(line):
                 x,y,z = getValues(line)
-                final.write(str(x) + ', ' + str(y) + ',' + str(z))
+                convertedX, convertedY, convertedZ = getTupleVelocity(x,y,z)
+                
+                final.write(str(convertedX) + ', ' + str(convertedY) + ',' + str(convertedZ) + "\n")
     print("DONE")
 
 main()
