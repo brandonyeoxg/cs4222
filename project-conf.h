7bc1accd4f2454efef6c55c81b91e4ecab30d571
Brandon Yeo <brandonyeoxg@gmail.com>
2018-03-14 16:44:54 +0800

    Config proj conf

--- project-conf.h
+++ project-conf.h
@@ -1,6 +1,6 @@
 #define PROJECT_CONF_H_
 #define RF_CORE_CONF_CHANNEL 13
-#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 128
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
 #define NETSTACK_NETWORK rime_driver
 #define NETSTACK_CONF_MAC nullmac_driver
 #define NETSTACK_CONF_RDC contikimac_driver
