4b3b6c3329ce16931e83aa04e6f543b5eb20b754
Brandon Yeo <brandonyeoxg@gmail.com>
2018-03-15 12:55:51 +0800

    Edit rcv

--- hw3-runicast.c
+++ hw3-runicast.c
@@ -56,8 +56,8 @@
 #define TRANSMISSION_DELAY  0.0001 * CLOCK_SECOND
 
 /* RCV addr */
-#define RCV_ADDR_0          179
-#define RCV_ADDR_1          130
+#define RCV_ADDR_0          88
+#define RCV_ADDR_1          0
 
 #define DEBUG
 
