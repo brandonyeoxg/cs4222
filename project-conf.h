0a302d89d2efad30033ecfe71b68764d9a8393e2
Brandon Yeo <brandonyeoxg@gmail.com>
2018-03-15 12:10:15 +0800

    Edit project config

--- project-conf.h
+++ project-conf.h
@@ -1,7 +1,7 @@
 #define PROJECT_CONF_H_
-#define RF_CORE_CONF_CHANNEL 13
-#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
+#define RF_CORE_CONF_CHANNEL 15
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 32
 #define NETSTACK_NETWORK rime_driver
 #define NETSTACK_CONF_MAC nullmac_driver
-#define NETSTACK_CONF_RDC contikimac_driver
+#define NETSTACK_CONF_RDC nullrdc_driver
 #define NETSTACK_CONF_FRAMER framer_802154
