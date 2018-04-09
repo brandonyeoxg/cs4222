6dda0d4ea0843ac2f12edfaceb36c6102bad73be
brandonyeoxg <brandonyeoxg@gmail.com>
2018-04-09 22:07:48 +0800

    Clean up working directory

--- floor_detector.py
+++
@@ -1,6 +0,0 @@
-class FloorDetector:
-	def __init__(self, samples):
-		pass
-
-	def compute(self):
-		pass
--- indoor_detector.py
+++
@@ -1,6 +0,0 @@
-class IndoorDetector:
-	def __init__(self, lightData, humidityData):
-		pass
-
-	def compute(self):
-		pass
