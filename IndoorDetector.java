049c868c0f0f84e697631d0ae49b82e7bc7fc15b
Gerald Wong <e0003102@u.nus.edu>
2018-04-19 02:38:10 +0800

    Remove compute method code for now, add new attributes to prepare for window, add calculatemean and calculatestandarddev method

--- IndoorDetector.java
+++ IndoorDetector.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class IndoorDetector {
     public static final float CONFIDENCE_LEVEL_THRESHOLD = 0.6f;
@@ -9,102 +10,66 @@ public class IndoorDetector {
     public static float LIGHT_WEIGHTING = 0.60f;
     public static float TEMP_WEIGHTING = 0.2f;
     public static float HUMID_WEIGHTING = 0.2f;
-    public static final int WINDOW_SIZE = 3;
+
+    private final int WINDOW_SIZE = 3;
+    private final float LIGHT_STANDARDDEV_THRESHOLD = 150.0f;
+    private final float HUMID_STANDARDDEV_THRESHOLD = 10.0f;
+    private final float TEMPERATURE_STANDARDDEV_THRESHOLD = 2.0f;
+
+    private String state, lastKnownState;
+    private ArrayList<ActivityData> lightSamples, humiditySamples, temperatureSamples;
+    private int lightSamplesStartIndex, lightSamplesEndIndex;
+    private int humiditySamplesStartIndex, humiditySamplesEndIndex;
+    private int temperatureSamplesStartIndex, temperatureSamplesEndIndex;
+    private int lightTotalSampleSize, humidityTotalSampleSize, temperatureTotalSampleSize;
     
     public IndoorDetector() {
+        state = ActivityState.IDLE;
+        lastKnownState = state;
+
+        lightSamples = new ArrayList<ActivityData>;
+        humiditySamples = new ArrayList<ActivityData>;
+        temperatureSamples = new ArrayList<ActivityData>;
+
+        lightSamplesStartIndex = 0;
+        lightSamplesEndIndex = 0;
+        humiditySamplesStartIndex = 0;
+        humiditySamplesEndIndex = 0;
+        temperatureSamplesStartIndex = 0;
+        temperatureSamplesEndIndex = 0;
+
+        lightTotalSampleSize = 0;
+        humidityTotalSampleSize = 0;
+        temperatureTotalSampleSize = 0;
+
         System.out.println("Hello IndoorDetector here");
     }
     
     public OutputState compute(ArrayList<ActivityData> tempList, ArrayList<ActivityData> lightList, ArrayList<ActivityData> humidList) {
-        float tempConfidence = 0.0f;
-        float lightConfidence = 0.0f;
-        float humidConfidence = 0.0f;
-        float totalConfidence = 0.0f;
-        
-        ArrayList<OutputState> confirmedResults = new ArrayList<OutputState>();
-        
-        ArrayList<Float> predictIndoor = new ArrayList<Float>();
-        
-        for (int i = 0; i < lightList.size(); i++) {
-            ActivityData lightData = lightList.get(i);
-            float lightCurrent = lightData.data.get(0);
-            if ((lightCurrent > OUTDOOR_LIGHT_THRESHOLD)) {
-                OutputState tentative = new OutputState(lightData.timestamp, ActivityState.OUTDOOR);
-                return tentative;
-            } else if ((lightCurrent <= OUTDOOR_LIGHT_THRESHOLD) && (lightCurrent > INDOOR_LIGHT_THRESHOLD)) {
-                lightConfidence = computeConfidenceLevel(lightCurrent, OUTDOOR_LIGHT_THRESHOLD);
-            } else if (lightCurrent <= INDOOR_LIGHT_THRESHOLD) {
-                lightConfidence = 1 - computeConfidenceLevel(lightCurrent, INDOOR_LIGHT_THRESHOLD);
-            }
-        }
-        predictIndoor.add(lightConfidence * LIGHT_WEIGHTING);
-        
-        
-        for (int j = 0; j < tempList.size(); j++) {
-            ActivityData tempData = tempList.get(j);
-            float tempCurrent = tempData.data.get(0);
-            if (tempCurrent <= TEMPERATURE_THRESHOLD) {
-                tempConfidence = 1;
-                return new OutputState(tempData.timestamp, ActivityState.INDOOR);
-            } else {
-                tempConfidence = 1 - computeConfidenceLevel(tempCurrent, TEMPERATURE_THRESHOLD);
-                predictIndoor.add(tempConfidence * TEMP_WEIGHTING);
-            }
-        }
-        
-        for (int k = 0; k < humidList.size(); k++) {
-            ActivityData humidData = humidList.get(k);
-            float humidCurrent = humidData.data.get(0);
-            if (humidCurrent <= HUMID_THRESHOLD) {
-                humidConfidence = computeConfidenceLevel(humidCurrent, HUMID_THRESHOLD);
-                predictIndoor.add(humidConfidence * HUMID_WEIGHTING);
-            } else {
-                humidConfidence = 1 - computeConfidenceLevel(humidCurrent, HUMID_THRESHOLD);
-                predictIndoor.add(humidConfidence * HUMID_WEIGHTING);
-            }
-        }
-        
-        totalConfidence = getSumOfArrayList(predictIndoor);
-        int timestamp = getMeanTimeStamp(tempList, lightList, humidList);
-        if (timestamp != -1) {
-            if (totalConfidence > CONFIDENCE_LEVEL_THRESHOLD){
-                return new OutputState(timestamp, ActivityState.INDOOR);
-            } else if ((1 - totalConfidence) > CONFIDENCE_LEVEL_THRESHOLD) {
-                return new OutputState(timestamp, ActivityState.OUTDOOR);
-            } else {
-                return new OutputState(-1, ActivityState.INDOOR);
-            }
-        } else {
-            return new OutputState(-1, ActivityState.OUTDOOR);
-        }
     }
-    private float computeConfidenceLevel(float reading, float threshold) {
-        return Math.abs((threshold - reading) / threshold);
-    }
-    private float getSumOfArrayList(ArrayList<Float> list) {
-        float sum = 0;
-        for (int i = 0; i < list.size(); i++) {
-            sum += list.get(i);
+
+    private float calculateStandardDev(ArrayList<ActivityData> dataList, int startIndex, int endIndex) {
+        int numOfData = endIndex - startIndex;
+        float meanOfData = calculateMean(dataList, startIndex, endIndex);
+        float summationOfDataMinusMeanSquared = 0.0f;
+        for (int i = startIndex; i < endIndex; i++) {
+            float currentData = dataList.get(i).data.get(0);
+            float dataMinusMeanSquared = (currentData - meanOfData) * (currentData - meanOfData);
+            summationOfDataMinusMeanSquared += dataMinusMeanSquared;
         }
-        return sum;
+        float standardDev = (float)Math.sqrt(summationOfDataMinusMeanSquared / numOfData);
+        return standardDev;
     }
-    private int getMeanTimeStamp(ArrayList<ActivityData> tempList, ArrayList<ActivityData> lightList, ArrayList<ActivityData> humidList) {
-        int totalNumberOfElements = tempList.size() + lightList.size() + humidList.size();
-        int answer = 0;
-        if (totalNumberOfElements != 0) {
-            for (int i = 0; i < tempList.size(); i++) {
-                answer += tempList.get(i).timestamp;
-            }
-            for (int j = 0; j < lightList.size(); j++) {
-                answer += lightList.get(j).timestamp;
-            }
-            for (int k = 0; k < humidList.size(); k++) {
-                answer += humidList.get(k).timestamp;
-            }
-            return Math.round(answer / totalNumberOfElements);
-        } else {
-            return -1;
+
+    private float calculateMean(ArrayList<ActivityData> dataList, int startIndex, int endIndex) {
+        int numOfData = endIndex - startIndex;
+        float sumOfAllData = 0.0f;
+        for (int i = startIndex; i < endIndex; i++) {
+            float currentData = dataList.get(i).data.get(0);
+            sumOfAllData += currentData;
         }
+        float meanOfAllData = sumOfAllData / numOfData;
+        return meanOfAllData;
     }
 }
 
